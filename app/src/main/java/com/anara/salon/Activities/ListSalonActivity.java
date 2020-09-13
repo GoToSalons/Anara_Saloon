253215e87557313a7975d5f17c42342e303d6706
Abdul <abdulkhira7569@gmail.com>
2020-09-13 15:01:45 +0530

    sorting

--- app/src/main/java/com/anara/salon/Activities/ListSalonActivity.java
+++ app/src/main/java/com/anara/salon/Activities/ListSalonActivity.java
@@ -1,6 +1,7 @@
 package com.anara.salon.Activities;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,9 +15,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.anara.salon.Adapters.SalonListAdapter;
 import com.anara.salon.ApiResponse.BaseRs;
+import com.anara.salon.ApiResponse.SalonModel;
 import com.anara.salon.Apis.Const;
 import com.anara.salon.Apis.RequestResponseManager;
 import com.anara.salon.Dialogs.SortDialog;
+import com.anara.salon.Helpers.GpsLocation;
 import com.anara.salon.R;
 import com.google.gson.Gson;
 
@@ -25,6 +28,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ListSalonActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -38,6 +43,9 @@ public class ListSalonActivity extends AppCompatActivity implements View.OnClick
     public JSONArray validFor;
     public JSONArray Rating;
     public JSONArray priceRange;
+    int distancePreference = 10000;
+
+    ArrayList<SalonModel> salonModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,25 +98,67 @@ public class ListSalonActivity extends AppCompatActivity implements View.OnClick
         }
     }
     public void getSalons(){
-        JSONObject parameters = new JSONObject();
-        try {
-            parameters.put("service_id", serviceId);
-            parameters.put("sort_by", sort);
-            parameters.put("valid_for", validFor);
-            parameters.put("rating", Rating);
-            parameters.put("price_range", priceRange);
-            Log.e("-=-=-=-=-=-","-=-=-=-=-=-"+parameters);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
-        RequestResponseManager.getSalon(parameters, Const.Get_Salon_Request, response -> {
-            if (response != null) {
-                BaseRs baseRs = (BaseRs) response;
-                SalonListAdapter salonListAdapter = new SalonListAdapter(ListSalonActivity.this, baseRs.getSaloons());
-                recyclerView.setAdapter(salonListAdapter);
+        if (sort.equals("near")) {
+            sortByNearByMe();
+        } else if (sort.equals("ratings")) {
+            sortByRatings();
+        } else {
+            JSONObject parameters = new JSONObject();
+            try {
+                parameters.put("service_id", serviceId);
+                parameters.put("sort_by", sort);
+                parameters.put("valid_for", validFor);
+                parameters.put("rating", Rating);
+                parameters.put("price_range", priceRange);
+                Log.e("-=-=-=-=-=-", "-=-=-=-=-=-" + parameters);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        });
+
+
+            RequestResponseManager.getSalon(parameters, Const.Get_Salon_Request, response -> {
+                if (response != null) {
+                    BaseRs baseRs = (BaseRs) response;
+                    salonModels = baseRs.getSaloons();
+                    SalonListAdapter salonListAdapter = new SalonListAdapter(ListSalonActivity.this, baseRs.getSaloons());
+                    recyclerView.setAdapter(salonListAdapter);
+                }
+            });
+        }
     }
+
+    private void sortByRatings() {
+        if (salonModels != null && salonModels.size() > 1) {
+            Collections.sort(salonModels, (salonModel, t1) -> salonModel.getRatings().compareTo(t1.getRatings()));
+            SalonListAdapter salonListAdapter = new SalonListAdapter(ListSalonActivity.this, salonModels);
+            recyclerView.setAdapter(salonListAdapter);
+        }
+    }
+
+    public void sortByNearByMe() {
+        if (salonModels != null && salonModels.size() > 1) {
+            Collections.sort(salonModels, (salonModel, t1) -> {
+                if (!salonModel.getLatitude().equals("") && !salonModel.getLogitude().equals("") && !t1.getLogitude().equals("") && !t1.getLatitude().equals("")) {
+                    Float distance1 = checkNearByMe(Double.parseDouble(salonModel.getLatitude()), Double.parseDouble(salonModel.getLogitude()));
+                    Float distance2 = checkNearByMe(Double.parseDouble(t1.getLatitude()), Double.parseDouble(t1.getLogitude()));
+                    return distance1.compareTo(distance2);
+                }
+                return 0;
+            });
+            Log.e("tag"," = = size  = = " + salonModels.size());
+            SalonListAdapter salonListAdapter = new SalonListAdapter(ListSalonActivity.this, salonModels);
+            recyclerView.setAdapter(salonListAdapter);
+        }
+    }
+
+    public float checkNearByMe(double latitude, double longtitude) {
+        GpsLocation gps = new GpsLocation(this);
+        double centerLatitude = gps.getLatitude();
+        double centerLongitude = gps.getLongitude();
+
+        float[] results = new float[1];
+        Location.distanceBetween(centerLatitude, centerLongitude, latitude, longtitude, results);
+        return results[0];
+    }
+
 }
--- app/src/main/java/com/anara/salon/Dialogs/SortDialog.java
+++ app/src/main/java/com/anara/salon/Dialogs/SortDialog.java
@@ -80,22 +80,23 @@ public class SortDialog extends DialogFragment implements View.OnClickListener {
 
         if (v.getId() == R.id.near_me) {
             listSalonActivity.serviceSort.setText("Near Me");
-            dismiss();
+            listSalonActivity.sort = "near";
+            listSalonActivity.getSalons();
         } else if (v.getId() == R.id.ratings) {
             listSalonActivity.serviceSort.setText("Ratings");
-            dismiss();
+            listSalonActivity.sort = "ratings";
+            listSalonActivity.getSalons();
         } else if (v.getId() == R.id.low_high) {
             listSalonActivity.serviceSort.setText("Price Low to High");
             listSalonActivity.sort = "price_low_high";
             listSalonActivity.getSalons();
-            dismiss();
         } else if (v.getId() == R.id.high_low) {
             listSalonActivity.serviceSort.setText("Price High to Low");
             listSalonActivity.sort = "price_high_low";
             listSalonActivity.getSalons();
-            dismiss();
         } else if (v.getId() == R.id.dismiss_l) {
             dismiss();
         }
+        dismiss();
     }
 }
